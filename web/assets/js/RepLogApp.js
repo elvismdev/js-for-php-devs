039756c8bfed76744f879b43129c8aa3b13ad1a1
Elvis Morales <elvismdev@gmail.com>
2018-06-21 00:12:33 -0700

    Using FOSJsRoutingBundle

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,8 +1,10 @@
 'use strict';
-(function(window, $) {
+(function(window, $, Routing) {
     window.RepLogApp = function($wrapper) {
         this.$wrapper = $wrapper;
         this.helper = new Helper($wrapper);
+
+        this.loadRepLogs();
 
         this.$wrapper.on(
             'click',
@@ -27,6 +29,18 @@
 
         _selectors: {
             newRepForm: '.js-new-rep-log-form'
+        },
+
+        loadRepLogs: function() {
+            var self = this;
+            $.ajax({
+                url: Routing.generate('rep_log_list'),
+                success: function(data) {
+                    $.each(data.items, function(key, repLog) {
+                        self._addRow(repLog);
+                    });
+                }
+            });
         },
 
         updateTotalWeightLifted: function() {
@@ -147,4 +161,4 @@
         }
     });
 
-})(window, jQuery);
+})(window, jQuery, Routing);
